0647d312ec15dbbcb6802eb74afd4164b4d44f0e
80130623 <silaspcosta@correios.com.br>
2020-03-28 01:56:37 -0300

    Front-end do app

--- backend/src/controllers/SessionController.js
+++ backend/src/controllers/SessionController.js
@@ -12,7 +12,8 @@ module.exports = {
         if(!ong) {
             return response.status(400).json({ error: 'No ONG found with this ID' })
         }
-
-        return response.json({ ong });
+        
+        const name = ong.name;
+        return response.json({ name });
     }
 }
